a55adcbb50f4d177d4b43aa8e5c886da23a9d8a3
Eric Pacuit <epacuit@umd.edu>
2023-05-10 09:33:37 -0400

    clean docs

--- docs/build/html/margin_graphs_examples/mg_ex_copeland_llull.py
+++
@@ -1,5 +0,0 @@
-from pref_voting.profiles import Profile
-            
-prof = Profile([[1, 3, 0, 4, 2], [0, 1, 4, 2, 3], [2, 4, 0, 1, 3], [3, 0, 2, 4, 1],  [4, 3, 1, 0, 2], [2, 3, 0, 1, 4]], [1, 1, 1, 1, 1, 1])
-
-prof.display_margin_graph()
--- docs/build/html/margin_graphs_examples/mg_ex_rp_with_test.py
+++
@@ -1,6 +0,0 @@
-
-from pref_voting.weighted_majority_graphs import MarginGraph
-
-mg = MarginGraph([0, 1, 2, 3], [(1, 2, 2), (1, 3, 2), (2, 0, 2)])
-
-mg.display()
