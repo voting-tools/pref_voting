d08b41a8735fcb6948d6e21f8ef8a5c65d0c2f3c
Eric Pacuit <epacuit@umd.edu>
2024-05-23 16:57:31 -0400

    add filelock to requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Wes Holliday and Eric Pacuit'
 author = 'Wes Holliday and Eric Pacuit'
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.4'
+release = '1.3.5'
 
 
 # -- General configuration ---------------------------------------------------
