9392d9d85d1b00ed20c7f09ad3a0ff495abde361
Eric Pacuit <epacuit@umd.edu>
2025-01-16 15:36:54 -0500

    update to version 1.15.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, Wes Holliday and Eric Pacuit'
 author = 'Wes Holliday and Eric Pacuit'
 
 # The full version, including alpha/beta/rc tags
-release = '1.14.30'
+release = '1.15.0'
 
 
 # -- General configuration ---------------------------------------------------
