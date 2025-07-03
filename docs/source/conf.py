90e20d060bbb7ce498e34cf055f3ebbeabede366
Eric Pacuit <epacuit@umd.edu>
2025-07-03 14:25:29 -0400

    fix tests for profile with ties

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, Wes Holliday and Eric Pacuit'
 author = 'Wes Holliday and Eric Pacuit'
 
 # The full version, including alpha/beta/rc tags
-release = '1.16.14'
+release = '1.16.20'
 
 
 # -- General configuration ---------------------------------------------------
