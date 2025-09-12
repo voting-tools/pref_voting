db9703d976f0697f4537b77529d5ffce1517babc
Eric Pacuit <epacuit@umd.edu>
2025-09-12 15:29:10 -0400

    update copeland docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, Wes Holliday and Eric Pacuit'
 author = 'Wes Holliday and Eric Pacuit'
 
 # The full version, including alpha/beta/rc tags
-release = '1.16.26'
+release = '1.16.28'
 html_extra_path = ['../../axiom_tables']
 
 
